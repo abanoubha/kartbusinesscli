02a53558e751f29413c1f8289ef6878c6c6cecba
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-05-26 15:13:43 +0300

    feature: (WIP) get index of cards

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -58,5 +60,26 @@ func searchCards(search string) {
 }
 
 func syncData() {
+	getCards()
+}
 
+func getCards() {
+	resp, err := http.Get("https://kartbusiness.com/api/v1/index?page=1")
+
+	if err != nil {
+		fmt.Println("Error: can not reach API endpoint", err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println(resp.Status, resp.Header)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body", err.Error())
+		return
+	}
+
+	fmt.Println(body)
 }
