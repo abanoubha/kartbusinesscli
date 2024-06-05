adc055b3ecb45f8a2a817a680589d9dd66e87092
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-06-05 14:32:06 +0300

    refactor: separate file for consuming API

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -61,25 +59,4 @@ func searchCards(search string) {
 
 func syncData() {
 	getCards()
-}
-
-func getCards() {
-	resp, err := http.Get("https://kartbusiness.com/api/v1/index?page=1")
-
-	if err != nil {
-		fmt.Println("Error: can not reach API endpoint", err.Error())
-		return
-	}
-
-	defer resp.Body.Close()
-
-	fmt.Println(resp.Status, resp.Header)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body", err.Error())
-		return
-	}
-
-	fmt.Println(body)
 }
