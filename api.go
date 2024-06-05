77473f15c3c7595858df6017d855363e4c4a7571
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-06-05 15:52:15 +0300

    feature: consuming index cards API endpoint

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,7 +10,7 @@ import (
 	"strconv"
 )
 
-// var db *sql.DB
+var db *sql.DB
 
 type ApiResponse struct {
 	Data []map[string]interface{} `json:"data"`
@@ -44,16 +45,16 @@ func getCards() {
 
 	card := make(map[string]interface{})
 
-	// tx, err := db.Begin()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// stmt, err := tx.Prepare(`INSERT OR IGNORE INTO cards (id, cat_id, gov_id, city_id, slug, name, slogan, mob, whatsapp, mail, web, blog, created_at,updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer stmt.Close()
+	stmt, err := tx.Prepare(`INSERT OR IGNORE INTO cards (id, cat_id, country_id, gov_id, city_id, slug, name, slogan, mob, whatsapp, mail, web, blog, created_at,updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
 
 	for _, v := range apiResponse.Data {
 
@@ -66,7 +67,7 @@ func getCards() {
 		if err != nil {
 			log.Fatalf("Failed to convert id to integer: %v", err)
 		}
-		fmt.Printf("id: %T\n", id)
+		// fmt.Printf("id: %T\n", id)
 
 		// cat_id
 		value, ok = v["cat_id"].(string)
@@ -77,7 +78,7 @@ func getCards() {
 		if err != nil {
 			log.Fatalf("Failed to convert cat_id to integer: %v", err)
 		}
-		fmt.Printf("cat_id: %T\n", cat_id)
+		// fmt.Printf("cat_id: %T\n", cat_id)
 
 		// country_id
 		value, ok = v["country_id"].(string)
@@ -88,7 +89,7 @@ func getCards() {
 		if err != nil {
 			log.Fatalf("Failed to convert country_id to integer: %v", err)
 		}
-		fmt.Printf("country_id: %T\n", country_id)
+		// fmt.Printf("country_id: %T\n", country_id)
 
 		// gov_id
 		value, ok = v["gov_id"].(string)
@@ -99,7 +100,7 @@ func getCards() {
 		if err != nil {
 			log.Fatalf("Failed to convert gov_id to integer: %v", err)
 		}
-		fmt.Printf("gov_id: %T\n", gov_id)
+		// fmt.Printf("gov_id: %T\n", gov_id)
 
 		// city_id
 		var city_id int
@@ -115,15 +116,31 @@ func getCards() {
 		} else {
 			city_id = 1
 		}
-		fmt.Printf("city_id: %T\n", city_id)
+		// fmt.Printf("city_id: %T\n", city_id)
 
-		fmt.Printf("slug: %T\n", v["slug"])
+		// fmt.Printf("slug: %T\n", v["slug"])
+		// fmt.Printf("name: %T\n", v["name"])
+		// fmt.Printf("slogan: %T\n", v["slogan"])
+		// fmt.Printf("mob: %T\n", v["mob"])
+		// fmt.Printf("created_at: %T\n", v["created_at"])
+		// fmt.Printf("updated_at: %T\n", v["updated_at"])
 
-		fmt.Printf("name: %T\n", v["name"])
-		fmt.Printf("slogan: %T\n", v["slogan"])
-		fmt.Printf("mob: %T\n", v["mob"])
-		fmt.Printf("created_at: %T\n", v["created_at"])
-		fmt.Printf("updated_at: %T\n", v["updated_at"])
+		_, err = stmt.Exec(
+			id, cat_id, gov_id, city_id, country_id,
+			v["slug"], v["name"], v["slogan"],
+			v["mob"], v["whatsapp"],
+			v["mail"], v["web"], v["blog"],
+			v["created_at"], v["updated_at"])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// commit all cards (50) to local SQLite db
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println(card)
